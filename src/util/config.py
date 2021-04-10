a2296fa48ca15743a1cc632c516a3863405f98f9
atyu1 <attila.sovak@gmail.com>
2021-04-10 15:39:18 +0200

    Add config reader and base sample file

--- src/util/config.py
+++ src/util/config.py
@@ -1,26 +1,23 @@
 import yaml
 
 class ConfigReader:
-    """Config file reader
-    Reads the content in config yaml file and based on that provides info
-    """
+  """
+  Reads the content in config yaml file and based on that provides info
+  """
 
-    def __init__(self, config_file="./config.yaml"):
-        """ Config file initiator """
-        self._config_file = config_file
+  def __init__(self, config_file="./config.yaml"):
+    """ Config file initiator """
+    self._config_file = config_file
+    self._read_config_file()
 
-        self._read_config_file()
+  def _read_config_file(self):
+    """ Read the config file content and save to variables """
 
-    def _read_config_file(self):
-        """ Read the config file content and save to variables """
+    with open(self._config_file, "r") as CONF_FILE:
+      try:
+        self._yaml_content = yaml.safe_load(CONF_FILE)
+      except yaml.YAMLError as exc:
+        print("Change me to log: ERROR: %s" % (exc,))
 
-        with open(self._config_file, "r") as CONF_FILE:
-            try:
-                self._yaml_content = yaml.safe_load(CONF_FILE)
-            except yaml.YAMLError as exc:
-                print("Change me to log: ERROR: %s" % (exc,))
-
-        self.url = self._yaml_content["url"]
-        self.tokenuser = self._yaml_content["tokenuser"]
-        self.tokenpass = self._yaml_content["tokenpass"]
-        self.sensors = self._yaml_content["sensors"]
+    self.tokenuser = self._yaml_content["tokenuser"]
+    self.tokenpass = self._yaml_content["tokenpass"]
