a4e630c8e97a068471efcb3dbb2baf03c4a73e66
atyu1 <attila.sovak@gmail.com>
2021-04-10 15:54:49 +0200

    Add base dht11 class

--- src/app.py
+++ src/app.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-from sensors.humidity_temperature import HumiditySensor as HUMIDITY
-from sensors.humidity_temperature import TemperatureSensor as TEMPERATURE
-from util.config import ConfigReader
+from sensors.dht11 import Dht11_Sensor as Sensor 
+from dotenv import load_dotenv
+import os
+import logger
 
 def app():
-    config_content = ConfigReader()
+     
+    load_dotenv()
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
     data = []
-    sensors = sensor_modularize(config_content)
 
-    for sensor in sensors:
-        data.append(sensor.run())
+    Sensor()
+    data.append(Sensor.run())
 
-
-    print (data)
     sender = JsonSender("https://localhost", data)
     sender.login(config_content.tokenuser, config_content.tokenpass)
     sender.push(data)
