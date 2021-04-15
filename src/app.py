9d5e6c4ae1bd6eaffc6de8f898c0cb631a5baf25
atyu1 <attila.sovak@gmail.com>
2021-04-15 13:22:02 +0200

    Add file reader and fix old code

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from sensor.dht_11 import DHT11_Sensor as Sensor 
+from handler import FileHandler as Handler
 from dotenv import load_dotenv
 import os
 import logger
@@ -9,15 +10,17 @@ def app():
      
     load_dotenv()
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    HUMIDITY_FILE = os.getenv("HUMIDITY_FILE")
+    TEMPERATURE_FILE = os.getenv("TEMPERATURE_FILE")
 
-    data = []
+    dht = Sensor()
+    data = dht.run()
 
-    Sensor()
-    data.append(Sensor.run())
+    temp_file = Handler(HUMIDITY_FILE)
+    hum_file = Handler(TEMPERATURE_FILE)
 
-    sender = JsonSender("https://localhost", data)
-    sender.login(config_content.tokenuser, config_content.tokenpass)
-    sender.push(data)
+    temp_file.save(data["temperature"])
+    hum_file.save(data["humidity"])
 
 if __name__ == "__main__":
 	app()
